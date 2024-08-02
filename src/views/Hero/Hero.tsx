fe51bc041c0be88a59a8bce2e4e4829db5c79895
VijayKumar9389 <vijaymkumar2095@gmail.com>
2024-08-02 10:02:27 -0600

    update details

--- src/views/Hero/Hero.tsx
+++ src/views/Hero/Hero.tsx
@@ -6,7 +6,7 @@ const Hero = () => {
         <div className="hero-container">
             <div className="hero-content">
                 <h1 className="hero-title">Got a Vision? Let's Build <strong>Your Perfect Business Tool</strong>.</h1>
-                <img className="hero-image" data-aos="fade-up" src={Image} alt="Laptops" />
+                <img className="hero-image" src={Image} alt="Laptops" />
             </div>
         </div>
     );
